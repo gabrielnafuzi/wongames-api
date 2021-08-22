2a161a2648a60fcad84d69261f3d6ef1b74c9d9b
Gabriel Moraes <gabriel.moraes@ceresinvestimentos.com>
2021-08-22 01:56:53 -0300

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '8d744c3671628d522d7e7b0bb8f79097'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'fda6d6ff-42bc-49eb-b52f-1733c6d38fce'
+};
